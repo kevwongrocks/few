4952a2fb7baa0ea449cdbc205501f39fdd81f5f1
Kevin Wong <kevwongrocks@gmail.com>
2014-07-10 21:59:01 -0400

    added AttackConvert class + scale_anim.xml

---
+++ FEW/src/com/kevwong/few/AttackConvert.java
@@ -0,0 +1,37 @@
+package com.kevwong.few;
+
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AttackConvert {
+
+	public static void main(int[] attack, View v, Object[] p) {
+		
+		for(int i = 0; i <= attack.length-1; i++){
+				
+			if(attack[i] == 0) {
+				
+				Drawable d = v.getResources().getDrawable(R.drawable.sbtn_fire);
+				ImageView image = (ImageView) v.findViewById((Integer) p[i]);
+				image.setImageDrawable(d);
+				
+			} else if(attack[i] == 1) {
+				
+				Drawable d = v.getResources().getDrawable(R.drawable.sbtn_earth);
+				ImageView image = (ImageView) v.findViewById((Integer) p[i]);
+				image.setImageDrawable(d);
+				
+			} else if(attack[i] == 2) {
+				
+				Drawable d = v.getResources().getDrawable(R.drawable.sbtn_water);
+				ImageView image = (ImageView) v.findViewById((Integer) p[i]);
+				image.setImageDrawable(d);
+				
+			}
+			
+		}
+		
+	}
+	
+}
